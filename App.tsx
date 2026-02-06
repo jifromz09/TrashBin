1254fcae0eb51b15defce9917fc622d76e7dabdb
etv <etv@etv-3.local>
2026-02-06 15:58:55 +0800

    updates

--- App.tsx
+++ App.tsx
@@ -1,16 +1,12 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
+ * TrashBin App - Image Labeling with ML Kit
  *
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { StatusBar, useColorScheme } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import ImageLabelingComponent from './components/ImageLabeling';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -18,28 +14,9 @@ function App() {
   return (
     <SafeAreaProvider>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <ImageLabelingComponent />
     </SafeAreaProvider>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
